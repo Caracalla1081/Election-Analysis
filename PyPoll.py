dac8e0fd379691c5b5a86c56135b334db45eda9a
Caracalla1081 <Caracalla1081@gmail.com>
2023-01-06 12:15:09 -0500

    Module 3.4.3 Write files to Python

--- PyPoll.py
+++ PyPoll.py
@@ -8,10 +8,10 @@
 import csv
 import os
 
-
-#Assign a variable for the file to load and the path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-with open(file_to_load) as election_data:
-
-    # Print the file object.
-    print(election_data)
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement to open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+# Write the three counties to the file.
+    txt_file.write("Counties in the Election\n-----------------------------\nArapahoe\nDenver\nJefferson")
+    
