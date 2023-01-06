c2bb951db492bbfc3416973f501b4347d45f8baf
Caracalla1081 <Caracalla1081@gmail.com>
2023-01-06 11:55:49 -0500

    Indirect file_to_load code

--- PyPoll.py
+++ PyPoll.py
@@ -3,8 +3,15 @@
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
-# 5. THe winner of the election based on popular vote.
+# 5. The winner of the election based on popular vote.
 
-file_to_load = 'C:\Users\Upt0w\Election-Analysis\Resources\election_results.csv'
-with open("C:\Users\Upt0w\Election-Analysis\Resources\election_results.csv", 'r') as election_data:
+import csv
+import os
+
+
+#Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+with open(file_to_load) as election_data:
+
+    # Print the file object.
     print(election_data)
